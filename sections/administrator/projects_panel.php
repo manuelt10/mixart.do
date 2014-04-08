908da53733c395764067e725c6a2decc72ecff46
manuelt10 <manu.rosariob@gmail.com>
2014-04-08 11:49:16 -0400

    .

--- sections/administrator/projects_panel.php
+++ sections/administrator/projects_panel.php
@@ -1,25 +1,27 @@
 <div class="userPanelWrapper">
 	<a href="admin?form=newproject">New Project</a>
-	<div class="projectHeader">
-		<span>Project Name</span><span>Status</span><span>Action</span>
-	</div>
-	<div>
+	<table class="table">
+		<thead>
+			<th><strong>Project Name</strong></th>
+			<th><strong>Status</strong></th>
+			<th><strong>Action</strong></th>
+		</thead>
 		<?php 
 			$project = $db->selectRecord('project',NULL,NULL,Array('created_date' => 'desc'));
 			foreach($project->data as $p)
 			{
 				?>
-				<div>
-					<span><?php echo $p->name; ?></span>
-					<span><?php echo $p->status == 1 ? "Visible" : "Invisible" ?></span>
-					<span>
-						<a href="?form=add_img_project&id=<?php echo $p->idprojects ?>">Add images</a>
-						<a href="?form=esp_proj_img&id=<?php echo $p->idprojects ?>">Add Special Images</a>
-						<a href="?form=change_project&id=<?php echo $p->idprojects ?>">Change Project</a>
-					</span>
-				</div>
+				<tbody>
+					<td><?php echo $p->name; ?></td>
+					<td><?php echo $p->status == 1 ? "Visible" : "Invisible" ?></td>
+					<td>
+						<a class="btn btn-default" href="?form=add_img_project&id=<?php echo $p->idprojects ?>">Add images</a>
+						<a class="btn btn-default" href="?form=esp_proj_img&id=<?php echo $p->idprojects ?>">Add Special Images</a>
+						<a class="btn btn-default" href="?form=change_project&id=<?php echo $p->idprojects ?>">Change Project</a>
+					</td>
+				</tbody>
 				<?php
 			}
 		?>
-	</div>
+		</table>
 </div>
